8b82c4259bc88f07c692cd4f3d6eac4080c18f95
B3n9219 <benkirk1441@gmail.com>
2025-08-01 17:12:29 +0100

    done stuff

--- BST/index.js
+++ BST/index.js
@@ -1,4 +1,4 @@
-import { Tree } from "./Tree.js"
+import {Tree} from "./Tree.js"
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node === null) {
@@ -16,21 +16,43 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 function logNodeData(node) {
     console.log(node.data)
 }
+function doubleNodeData(node) {
+    node.data = node.data * 2
+}
 
-const tree= new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 8, 6345, 324])
+function createRandomTree() {
+    let array  = Array.from({ length: 20 }, () => Math.floor(Math.random() * 100) + 1);
+    return new Tree(array)
+}
+function addRandomNumbers(tree, num) {
+    for (let n = 0; n < num; n++) {
+        tree.insert(Math.floor(Math.random() * 100))
+    }
+}
+
+const tree= createRandomTree()
 tree.buildTree()
-
+addRandomNumbers(tree, 20)
 prettyPrint(tree.root)
-tree.insert(7000)
-prettyPrint(tree.root)
-tree.insert(8000)
-prettyPrint(tree.root)
-tree.insert(9000)
-prettyPrint(tree.root)
-console.log("BALANCED?", tree.isBalanced())
+console.log(tree.isBalanced())
 tree.rebalance()
 prettyPrint(tree.root)
-console.log(tree.depth(67))
+console.log(tree.isBalanced())
+tree.inOrderForEach(doubleNodeData)
+tree.inOrderForEach(logNodeData)
+prettyPrint(tree.root)
+
+
+// tree.insert(7000)
+// prettyPrint(tree.root)
+// tree.insert(8000)
+// prettyPrint(tree.root)
+// tree.insert(9000)
+// prettyPrint(tree.root)
+// console.log("BALANCED?", tree.isBalanced())
+// tree.rebalance()
+// prettyPrint(tree.root)
+// console.log(tree.depth(67))
 
 
 // prettyPrint(([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 8, 6345, 324]))
